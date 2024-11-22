68b7cd776b8cea15d199e0512a84e5286f14d252
lazcansar <56204736+lazcansar@users.noreply.github.com>
2024-11-22 10:58:21 +0300

    Step-2 Design

--- main.js
+++ main.js
@@ -32,3 +32,33 @@ document.querySelectorAll('.main-box-item').forEach(item => {
         });
     });
 });
+
+
+document.querySelectorAll('.main-box-card').forEach(item => {
+    item.addEventListener('click', function(e) {
+
+        // Mevcut seçili durumu kaldırın
+        document.querySelectorAll('.main-box-card').forEach(el => el.classList.remove('selected'));
+
+        // Tıklanan elemana 'selected' sınıfını ekleyin
+        this.classList.add('selected');
+
+
+        const ripple = document.createElement('span');
+        ripple.classList.add('ripple-card');
+
+        const rect = item.getBoundingClientRect();
+        const size = Math.max(rect.width, rect.height);
+        ripple.style.width = ripple.style.height = `${size * 2}px`;
+        ripple.style.left = `${e.clientX - rect.left - size}px`;
+        ripple.style.top = `${e.clientY - rect.top - size}px`;
+
+        // Ripple'ı kutuya ekle
+        item.appendChild(ripple);
+
+        // Ripple animasyonu bittikten sonra elementi kaldır
+        ripple.addEventListener('animationend', () => {
+            ripple.remove();
+        });
+    });
+});
