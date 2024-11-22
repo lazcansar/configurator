1cc0f97ed12e79cf1325cf26beed73cf8493bf40
Abdullah <abdullahgoksal@outlook.com>
2024-11-22 15:56:01 +0300

    Step-3 Design

--- main.js
+++ main.js
@@ -66,6 +66,36 @@ document.querySelectorAll('.main-box-card').forEach(item => {
     });
 });
 
+
+document.querySelectorAll('.main-box-sizing-list-item').forEach(item => {
+    item.addEventListener('click', function(e) {
+
+        // Mevcut seçili durumu kaldırın
+        document.querySelectorAll('.main-box-sizing-list-item').forEach(el => el.classList.remove('selected'));
+
+        // Tıklanan elemana 'selected' sınıfını ekleyin
+        this.classList.add('selected');
+
+
+        const ripple = document.createElement('span');
+        ripple.classList.add('ripple');
+
+        const rect = item.getBoundingClientRect();
+        const size = Math.max(rect.width, rect.height);
+        ripple.style.width = ripple.style.height = `${size * 2}px`;
+        ripple.style.left = `${e.clientX - rect.left - size}px`;
+        ripple.style.top = `${e.clientY - rect.top - size}px`;
+
+        // Ripple'ı kutuya ekle
+        item.appendChild(ripple);
+
+        // Ripple animasyonu bittikten sonra elementi kaldır
+        ripple.addEventListener('animationend', () => {
+            ripple.remove();
+        });
+    });
+});
+
 document.addEventListener('DOMContentLoaded', () => {
     // Select all main-box-card elements
     const cards = document.querySelectorAll('.main-box-card');
