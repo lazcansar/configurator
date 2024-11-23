23d447afa5fdd529b4b4dba0b3cc9eac1f969d5b
lazcansar <56204736+lazcansar@users.noreply.github.com>
2024-11-23 13:08:00 +0300

    Step-8 Design

--- main.js
+++ main.js
@@ -61,41 +61,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
-/*
-Step 1
- */
-document.querySelectorAll('.main-box-item').forEach(item => {
-    item.addEventListener('click', function(e) {
-
-        // Mevcut seçili durumu kaldırın
-        document.querySelectorAll('.main-box-item').forEach(el => el.classList.remove('selected'));
-
-        // Tıklanan elemana 'selected' sınıfını ekleyin
-        this.classList.add('selected');
-
-
-        const ripple = document.createElement('span');
-        ripple.classList.add('ripple');
-
-        const rect = item.getBoundingClientRect();
-        const size = Math.max(rect.width, rect.height);
-        ripple.style.width = ripple.style.height = `${size * 2}px`;
-        ripple.style.left = `${e.clientX - rect.left - size}px`;
-        ripple.style.top = `${e.clientY - rect.top - size}px`;
-
-        // Ripple'ı kutuya ekle
-        item.appendChild(ripple);
-
-        // Ripple animasyonu bittikten sonra elementi kaldır
-        ripple.addEventListener('animationend', () => {
-            ripple.remove();
-        });
-    });
-});
-
 /*
 Step 2
  */
